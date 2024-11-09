91893d315910fa0e9f0eb187c2780d334dd7b590
masoud <karimi.masoud5494@gmail.com>
2024-11-10 02:04:25 +0330

    add release build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,12 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
